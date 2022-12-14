f9431efd7a05ef3f6ee63a1845252aff90ab98ad
Mhossain028 <meraj.hossain028@gmail.com>
2022-12-15 00:14:36 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/algoramming/bg_remover/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.algoramming.bg_remover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/bg_remove_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BackgroundRemoveScreen extends StatelessWidget {
+  const BackgroundRemoveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
